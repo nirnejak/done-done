4b8c8059a3e569be0c32ff0dd9654318c8b2bb7c
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-23 15:26:14 +0530

    layout: zinc to neutral

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ const RootLayout: React.FC<Props> = ({ children }) => {
         <body
           className={classNames(
             sansFont.variable,
-            "overflow-x-hidden bg-zinc-50 dark:bg-zinc-900 font-sans"
+            "overflow-x-hidden bg-neutral-50 dark:bg-neutral-900 font-sans"
           )}
         >
           {children}
