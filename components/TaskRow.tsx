f136504e70db9318514193acae363cc3c8d4ffba
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-23 23:25:20 +0530

    Task List: UI and integration

---
+++ components/TaskRow.tsx
@@ -0,0 +1,108 @@
+"use client"
+import * as React from "react"
+
+import {
+  Pencil,
+  TrashBin,
+  DragVerticalFill,
+  Circle,
+  CircleCheckFill,
+} from "akar-icons"
+import { Reorder, useDragControls, useMotionValue } from "motion/react"
+
+import classNames from "@/utils/classNames"
+
+import type { TASK } from "@/context/TasksContext"
+
+interface Props {
+  task: TASK
+  editTask: (id: string) => void
+  deleteTask: (id: string) => void
+  toggleTask: (id: string) => void
+}
+
+const TaskRow: React.FC<Props> = ({
+  task,
+  editTask,
+  deleteTask,
+  toggleTask,
+}) => {
+  const y = useMotionValue(0)
+  const dragControls = useDragControls()
+
+  const style = { y }
+
+  return (
+    <Reorder.Item value={task} dragListener={false} dragControls={dragControls}>
+      <div
+        role="button"
+        tabIndex={0}
+        onKeyDown={(e) => {
+          e.key === "Enter" && toggleTask(task.id)
+        }}
+        onClick={(e) => {
+          toggleTask(task.id)
+        }}
+        className={classNames(
+          "group -mx-2 flex items-center rounded-lg p-1 outline-none transition-colors",
+          "text-neutral-700 hover:text-neutral-900 focus:text-neutral-900 hover:bg-neutral-200/50 focus:bg-neutral-200/50",
+          "dark:text-neutral-400 dark:hover:text-neutral-200 dark:focus:text-neutral-200 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
+        )}
+      >
+        <div className="my-0 flex items-center gap-1 truncate p-1 text-sm">
+          <input
+            tabIndex={-1}
+            type="checkbox"
+            className="mr-1 hidden accent-neutral-600"
+            id={"task-" + task.id}
+            checked={task.isCompleted}
+            onChange={() => {
+              toggleTask(task.id)
+            }}
+          />
+          {task.isCompleted ? (
+            <CircleCheckFill size={18} />
+          ) : (
+            <Circle size={18} />
+          )}
+          <label htmlFor={"task-" + task.id} className="ml-1 font-medium">
+            {task.title}
+          </label>
+        </div>
+        <div className="ml-auto flex items-center gap-2">
+          <div className="hidden group-hover:flex group-focus:flex">
+            <button
+              className="rounded p-1 text-neutral-400 transition-colors hover:bg-neutral-200 hover:text-neutral-600 focus:bg-neutral-200 focus:text-neutral-600 dark:hover:bg-neutral-900 dark:focus:bg-neutral-900"
+              onClick={(e) => {
+                e.stopPropagation()
+                editTask(task.id)
+              }}
+            >
+              <Pencil size={17} />
+            </button>
+            <button
+              className="rounded p-1 text-red-400 transition-colors hover:bg-red-100 hover:text-red-600 focus:bg-red-100 focus:text-red-600 dark:hover:bg-red-900 dark:focus:bg-red-900"
+              onClick={(e) => {
+                e.stopPropagation()
+                deleteTask(task.id)
+              }}
+            >
+              <TrashBin size={17} />
+            </button>
+          </div>
+          <div
+            className="cursor-grab text-neutral-400"
+            style={style as React.CSSProperties}
+            onPointerDown={(e) => {
+              dragControls.start(e)
+            }}
+          >
+            <DragVerticalFill size={15} />
+          </div>
+        </div>
+      </div>
+    </Reorder.Item>
+  )
+}
+
+export default TaskRow
--- components/Tasks.tsx
+++ components/Tasks.tsx
@@ -2,12 +2,19 @@
 import * as React from "react"
 import { redirect } from "next/navigation"
 
-import { useAuth } from "@/context/AuthContext"
+import { motion, Reorder } from "motion/react"
+import { Sort } from "akar-icons"
 
+import { useAuth } from "@/context/AuthContext"
+import { useTasks } from "@/context/TasksContext"
+
+import TaskRow from "@/components/TaskRow"
 import AddTask from "@/components/AddTask"
 
 const Tasks: React.FC = () => {
   const { user } = useAuth()
+  const { tasks, toggleTask, sortTasksAlphabetically, reorderTasks } =
+    useTasks()
 
   React.useEffect(() => {
     if (!user.isAuthenticated) {
@@ -15,12 +22,56 @@ const Tasks: React.FC = () => {
     }
   }, [user.isAuthenticated])
 
+  const openEditTaskModal = (id: string): void => {}
+
+  const deleteTask = (id: string): void => {}
+
   return (
     <section className="mx-auto mt-20 w-[500px] md:mt-40">
       <h1 className="mb-3 font-semibold text-neutral-700 dark:text-neutral-200">
         Hi {user.name} ;)
       </h1>
       <AddTask />
+      <motion.div
+        layout
+        className="rounded-3xl bg-neutral-200 p-2 shadow-heavy dark:bg-neutral-800 dark:shadow-md"
+      >
+        <motion.div
+          layout
+          className="rounded-2xl bg-neutral-50 px-4 pb-2 pt-4 shadow-heavy dark:bg-neutral-900 dark:shadow-md"
+        >
+          {tasks.length > 0 && (
+            <div className="mb-2 flex items-center text-neutral-500">
+              <p className="text-xs font-medium">Tasks</p>
+              <button
+                className="-mr-1 ml-auto rounded p-1 outline-none hover:bg-neutral-200/50 focus:bg-neutral-200/50"
+                onKeyDown={(e) => {
+                  e.key === "Enter" && sortTasksAlphabetically()
+                }}
+                onClick={() => {
+                  sortTasksAlphabetically()
+                }}
+              >
+                <Sort size={15} />
+              </button>
+            </div>
+          )}
+
+          <div className="flex w-full select-none flex-col">
+            <Reorder.Group axis="y" values={tasks} onReorder={reorderTasks}>
+              {tasks.map((task) => (
+                <TaskRow
+                  key={task.id}
+                  task={task}
+                  toggleTask={toggleTask}
+                  editTask={openEditTaskModal}
+                  deleteTask={deleteTask}
+                />
+              ))}
+            </Reorder.Group>
+          </div>
+        </motion.div>
+      </motion.div>
     </section>
   )
 }
