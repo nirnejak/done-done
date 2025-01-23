fe2b291c18157501721d38b9420d495a999b7025
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-23 19:20:24 +0530

    Tasks Page: tasks component added

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,9 +1,8 @@
 import * as React from "react"
 
-import * as motion from "motion/react-client"
-
-import { BASE_TRANSITION } from "@/utils/animation"
 import { generateMetadata } from "@/utils/metadata"
+
+import Tasks from "@/components/Tasks"
 
 export const metadata = generateMetadata({
   path: "/tasks",
@@ -11,19 +10,8 @@ export const metadata = generateMetadata({
   description: "A sleek and simple todo list app",
 })
 
-const Home: React.FC = () => {
-  return (
-    <main className="grid h-screen place-content-center">
-      <motion.h1
-        initial={{ translateY: 20, opacity: 0, filter: `blur(10px)` }}
-        animate={{ translateY: 0, opacity: 1, filter: "none" }}
-        transition={{ delay: 0, ...BASE_TRANSITION }}
-        className="text-5xl font-bold tracking-tighter text-neutral-800 dark:text-neutral-300"
-      >
-        Tasks
-      </motion.h1>
-    </main>
-  )
+const TasksPage: React.FC = () => {
+  return <Tasks />
 }
 
-export default Home
+export default TasksPage
