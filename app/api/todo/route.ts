7c34c86a98b0fd440cf653db0311b285db72715e
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-25 01:51:51 +0530

    Todos API implemented

---
+++ app/api/todo/route.ts
@@ -0,0 +1,100 @@
+import { NextRequest, NextResponse } from "next/server"
+
+import { and } from "drizzle-orm"
+
+import { db } from "@/drizzle.config"
+
+import { todos } from "@/app/api/schema"
+import { getUserIdFromRequestHeaderToken } from "@/app/api/utils"
+
+export async function GET(request: NextRequest) {
+  const userId = await getUserIdFromRequestHeaderToken(request)
+
+  try {
+    const allTodos = await db.select().from(todos).where({ user_id: userId })
+    return NextResponse.json(
+      allTodos.map((todo) => ({
+        ...todo,
+        due_date: todo.due_date?.toISOString().replace(/T.*/, ""),
+      }))
+    )
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(
+      { error: "Failed to fetch todos" },
+      { status: 500 }
+    )
+  }
+}
+
+export async function POST(request: NextRequest) {
+  const userId = await getUserIdFromRequestHeaderToken(request)
+
+  try {
+    const { title, description, due_date } = await request.json()
+    if (!title) {
+      return NextResponse.json({ error: "Title is required" }, { status: 400 })
+    }
+
+    const newTodo = await db
+      .insert(todos)
+      .values({ title, userId, description, due_date })
+      .$returningId()
+    return NextResponse.json({
+      id: newTodo[0].id,
+      title,
+      description,
+      due_date,
+    })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(
+      { error: "Failed to create todo" },
+      { status: 500 }
+    )
+  }
+}
+
+export async function DELETE(request: NextRequest) {
+  const userId = await getUserIdFromRequestHeaderToken(request)
+
+  try {
+    const { id } = await request.json()
+    if (!id) {
+      return NextResponse.json({ error: "ID is required" }, { status: 400 })
+    }
+
+    await db.delete(todos).where(and({ id }, { user_id: userId }))
+    return NextResponse.json({ success: true })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(
+      { error: "Failed to delete todo" },
+      { status: 500 }
+    )
+  }
+}
+
+export async function PUT(request: NextRequest) {
+  const userId = await getUserIdFromRequestHeaderToken(request)
+
+  try {
+    const { id, title, description, due_date } = await request.json()
+    if (!id) {
+      return NextResponse.json({ error: "ID is required" }, { status: 400 })
+    }
+
+    await db
+      .update(todos)
+      .set({ title, description, due_date })
+      .where(and({ id }, { user_id: userId }))
+
+    return NextResponse.json({ id, title, description, due_date })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(
+      { error: "Failed to update todo" },
+      { status: 500 }
+    )
+  }
+}
