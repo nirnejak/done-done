fd87112849dc4bcb79e505afde287eef5b36fac6
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-25 02:38:51 +0530

    Not Found: layout fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -12,24 +12,22 @@ export const metadata = generateMetadata({
 
 const NotFound: React.FC = () => {
   return (
-    <main className="container">
-      <section className="grid min-h-screen place-content-center">
-        <div className="text-center">
-          <h1 className="mb-3 text-9xl font-semibold tracking-tighter text-neutral-800 dark:text-neutral-300">
-            404
-          </h1>
-          <p className="text-base text-neutral-800 dark:text-neutral-300">
-            Page not found, go{" "}
-            <Link
-              href="/"
-              className="font-semibold hover:underline focus:underline"
-            >
-              Home
-            </Link>
-          </p>
-        </div>
-      </section>
-    </main>
+    <section className="grid min-h-screen place-content-center">
+      <div className="text-center">
+        <h1 className="mb-3 text-9xl font-semibold tracking-tighter text-neutral-800 dark:text-neutral-300">
+          404
+        </h1>
+        <p className="text-base text-neutral-800 dark:text-neutral-300">
+          Page not found, go{" "}
+          <Link
+            href="/"
+            className="font-semibold hover:underline focus:underline"
+          >
+            Home
+          </Link>
+        </p>
+      </div>
+    </section>
   )
 }
 
