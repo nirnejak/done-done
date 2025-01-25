a9d1d0bc49e08d24143115d3d59a554ca047c893
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-25 18:15:23 +0530

    datetime util: format style updated

--- utils/datetime.ts
+++ utils/datetime.ts
@@ -1,11 +1,11 @@
 import { format, formatDistance } from "date-fns"
 
 export const formatToDate = (date: Date): string => {
-  return format(date, "dd-MMM-yyyy")
+  return format(date, "dd MMM yyyy")
 }
 
 export const formatToDateTime = (date: Date): string => {
-  return format(date, "dd-MMM-yyyy: p")
+  return format(date, "dd MMM yyyy: p")
 }
 
 export const fromNow = (date: Date): string => {
