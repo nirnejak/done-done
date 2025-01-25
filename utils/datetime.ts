8c3dd3466dcf1ac48a29ba4158782150df18c508
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-25 19:56:28 +0530

    datetime: fromNow fixes

--- utils/datetime.ts
+++ utils/datetime.ts
@@ -1,4 +1,4 @@
-import { format, differenceInDays } from "date-fns"
+import { format, formatDistance, startOfDay } from "date-fns"
 
 export const formatToDate = (date: Date): string => {
   return format(date, "dd MMM yyyy")
@@ -9,20 +9,10 @@ export const formatToDateTime = (date: Date): string => {
 }
 
 export const fromNow = (date: Date): string => {
-  const daysDifference = differenceInDays(date, new Date())
-
-  switch (true) {
-    case daysDifference === 0:
-      return "Today"
-    case daysDifference === 1:
-      return "Yesterday"
-    case daysDifference === -1:
-      return "Tomorrow"
-    case daysDifference > 0:
-      return `${daysDifference} days ago`
-    case daysDifference < 0:
-      return `in ${Math.abs(daysDifference)} days`
-    default:
-      return ""
+  if (date.getDate() === new Date().getDate()) {
+    return "today"
   }
+  return formatDistance(startOfDay(date), new Date(), {
+    addSuffix: true,
+  })
 }
