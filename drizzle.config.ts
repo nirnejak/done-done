ea96ebb559defd2a395b79c018d1b4bc35bdbb82
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-01-26 15:14:35 +0530

    drizzle config updated for neondb

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,22 +1,15 @@
+import { neon } from "@neondatabase/serverless"
+import { drizzle } from "drizzle-orm/neon-http"
 import { defineConfig } from "drizzle-kit"
 
-import { drizzle } from "drizzle-orm/mysql2"
-import mysql from "mysql2/promise"
-
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-})
+const sql = neon(process.env.DATABASE_URL!)
+export const db = drizzle({ client: sql })
 
 export default defineConfig({
   schema: "**/schema.ts",
   out: "./drizzle",
-  dialect: "mysql",
+  dialect: "postgresql",
   dbCredentials: {
-    url: `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    url: process.env.DATABASE_URL as string,
   },
 })
-
-export const db = drizzle(pool)
